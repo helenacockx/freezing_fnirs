d059fe1a50e203c8bfc28e3f8f9d580f6623e58e
helenacockx <h.cockx@donders.ru.nl>
2020-11-09 11:59:22 +0100

    better selection of xdfevents

--- test_xdfdelay.m
+++ test_xdfdelay.m
@@ -1,18 +1,23 @@
 function test_xdfdelay(xdfevent)
 
-events{1}=[xdfevent(strcmp({xdfevent.type}, 'Digital Triggers @ lsldert00')).timestamp];
-events{1}=events{1}(1:end-1); % last event is empty
-events{2}=[xdfevent(strcmp({xdfevent.type}, 'Digital Triggers @ lsldert01')).timestamp];
-events{3}=[xdfevent(strcmp({xdfevent.type}, 'Digital Triggers @ lsldert02')).timestamp];
-events{4}=[xdfevent(strcmp({xdfevent.type}, 'Digital Triggers @ lsldert03')).timestamp];
-events{5}=[xdfevent(strcmp({xdfevent.type}, 'Digital Triggers @ lsldert04')).timestamp];
+xdfevent=xdfevent(contains({xdfevent.value}, {'TTL_on', 'TTL_off', 'start_run', 'stop_run', 'start_block', 'stop_block', 'sync'})); % only select relevant events
+lsldert00_events=ft_filter_event(xdfevent, 'type', 'Digital Triggers @ lsldert00');
+events{1}=[lsldert00_events.timestamp];
+lsldert01_events=ft_filter_event(xdfevent, 'type', 'Digital Triggers @ lsldert01');
+events{2}=[lsldert01_events.timestamp];
+lsldert02_events=ft_filter_event(xdfevent, 'type', 'Digital Triggers @ lsldert02');
+events{3}=[lsldert02_events.timestamp];
+lsldert03_events=ft_filter_event(xdfevent, 'type', 'Digital Triggers @ lsldert03');
+events{4}=[lsldert03_events.timestamp];
+lsldert04_events=ft_filter_event(xdfevent, 'type', 'Digital Triggers @ lsldert04');
+events{5}=[lsldert04_events.timestamp];
 
-figure;
+figure
 subplot(3,1,1); title('timestamps for all events'); hold on
 for i=1:5
   plot(events{i}, ones(size(events{i}))*i, '+')
 end
-ylim([-23 30])
+ylim([-3 10])
 legend({'lsldert00', 'lsldert01', 'lsldert02', 'lsldert03', 'lsldert04'}, 'Location', 'eastoutside')
 
 subplot(3,1,2); title('delay of timestamp relative to when the trigger was sent via lsldert00'); hold on;
@@ -20,8 +25,8 @@ for i=2:5
   plot(events{1}, (events{i}-events{1})*1000)
 end
 legend({'lsldert01', 'lsldert02', 'lsldert03', 'lsldert04'}, 'Location', 'eastoutside')
-ylabel('time delay (msec.)'); xlabel('timestamps');
-ylim([0 100]);
+ylabel('time delay (msec.)'); xlabel('timestamps');yticks([0:10:50]); grid on
+ylim([0 50]);
 
 subplot(3,1,3); title('delay between raspberry pies (relative to lsldert04)'); hold on;
 for i=2:4
@@ -31,6 +36,6 @@ for i=2:4
   end
 end
 legend({'lsldert01', 'lsldert02', 'lsldert03'}, 'Location', 'eastoutside')
-ylabel('time delay (msec.)'); xlabel('timestamps');
-ylim([0 100]);
+ylabel('time delay (msec.)'); xlabel('timestamps'); yticks([0:10:50]); grid on
+ylim([0 50]);
 
