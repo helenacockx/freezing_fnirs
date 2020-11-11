9c19e470f14644ff25f54596f5d9e3fbe0ef6df1
helenacockx <h.cockx@donders.ru.nl>
2020-11-11 13:35:38 +0100

    use REC for lsl events instead of .xdf files

--- load_data.m
+++ load_data.m
@@ -1,6 +1,6 @@
 %% initialisation
 close all; clear all
-ID='HC90';
+ID='PD11';
 sub=['sub-' ID];
 root_dir='C:\Users\helen\Documents\freezing_fnirs\data';
 addpath C:\Users\helen\Documents\MATLAB\matlab_toolboxes\fix_artinis
@@ -8,9 +8,9 @@ addpath(fullfile(root_dir, 'scripts'))
 dbstop if error
 % completed subjects: PD11, HC66
 
-runs=[5:8];
-num_run=8;
-num_block=12; 
+runs=[1:5];
+num_run=5;
+num_block=13; 
 
 %nirs
 delay_devices=0;
@@ -23,26 +23,30 @@ end
 % log file
 logname=fullfile(root_dir, 'processed', sub, sprintf('%s_load_data.log', sub)); diary(logname);
 
-
-%% load xdf files
+%% load REC variable
 fprintf('\n.........loading lsl data.........\n')
-xdffiles=dir(fullfile(root_dir, 'source_standard', sub, 'stim', '*triggerslabrecorder.xdf'));
-
-% read in events
-xdfevent=ft_read_event(fullfile(xdffiles.folder, xdffiles.name));
+triggerinfo=dir(fullfile(root_dir, 'source_standard', sub, 'stim', '*rec-0*_triggerinfo.mat'));
+load(fullfile(triggerinfo.folder, triggerinfo.name)); 
 
 % check number of events
-if ~isequal(sum(contains({xdfevent.value},'TTL_on')),sum(contains({xdfevent.value},'TTL_off')),sum(contains({xdfevent.value},'start_run')),sum(contains({xdfevent.value},'stop_run')), 5*num_run)
-  warning('The number of start_runs/TTL_on and stop_runs/TTL_off were not as expected')
+lsl_streams={'lsldert0_events', 'lsldert1_events', 'lsldert2_events', 'lsldert3_events', 'lsldert4_events'};
+sync=nan(1,length(lsl_streams));
+for i=1:length(lsl_streams)
+  if ~isequal(sum(contains(REC.(lsl_streams{i}).Data,'TTL_on')),sum(contains(REC.(lsl_streams{i}).Data,'TTL_off')),sum(contains(REC.(lsl_streams{i}).Data,'start_run')),sum(contains(REC.(lsl_streams{i}).Data,'stop_run')), num_run)
+    warning('The number of start_runs/TTL_on and stop_runs/TTL_off of %s were not as expected', lsl_streams{i})
+  end
+  if ~isequal(sum(contains(REC.(lsl_streams{i}).Data,'start_block')),sum(contains(REC.(lsl_streams{i}).Data,'stop_block')), num_block)
+    warning('The number of start_blocks and stop_blocks of %s were not as expected', lsl_streaminfos{i})
+  end
+  sync(i)=sum(contains(REC.(lsl_streams{i}).Data, 'sync'));
 end
-if ~isequal(sum(contains({xdfevent.value},'start_block')),sum(contains({xdfevent.value},'stop_block')), 5*num_block)
-  warning('The number of start_blocks and stop_blocks were not as expected')
-end
-if ~isequal(sum(contains({xdfevent.value}, 'sync') & strcmp({xdfevent.type}, 'Digital Triggers @ lsldert00')),sum(contains({xdfevent.value}, 'sync') & strcmp({xdfevent.type}, 'Digital Triggers @ lsldert01')),sum(contains({xdfevent.value}, 'sync') & strcmp({xdfevent.type}, 'Digital Triggers @ lsldert02')),sum(contains({xdfevent.value}, 'sync') & strcmp({xdfevent.type}, 'Digital Triggers @ lsldert03')), sum(contains({xdfevent.value}, 'sync') & strcmp({xdfevent.type}, 'Digital Triggers @ lsldert04')))
+if 
+if length(unique(sync))~=1
   warning('The number of sync events were not equal between the lsl streams')
 end
-% check delay between events
-test_xdfdelay(xdfevent);
+% check delay between events and calculate corrections
+lsl_events=test_delay(REC);
+
 
 %% load nirs data
 fprintf('\n.........loading nirs data.........\n')
@@ -55,28 +59,36 @@ save(fullfile(root_dir, 'processed', sub, 'nirs', sprintf('sub-%s_task-gait_rec-
 % check events
 [hdr, ~]=readoxy4(fullfile(oxy4files.folder, oxy4files.name));
 stream=hdr.oxy3.Device{3}.Attributes.desc;
-streamnumber=regexp(stream, '\d+', 'match');
-lslstream=sprintf('lsldert%s', streamnumber{1}); 
-lsl_nirsevents=xdfevent(contains({xdfevent.type}, lslstream)&contains({xdfevent.value}, {'start_run', 'stop_run', 'start_block', 'stop_block', 'sync'}));
+streamnumber=regexp(stream, '0(\d)', 'tokens');
+lslstream=sprintf('lsldert%s_events', streamnumber{1}{1}); 
+lsl_nirsevents=lsl_events(contains(lsl_events.type, lslstream)&contains(lsl_events.value, {'start_run', 'stop_run', 'start_block', 'stop_block', 'sync'}),:);
 oxy4_nirsevents=nirsevents(contains({nirsevents.value}, {'start_run', 'stop_run', 'start_block', 'stop_block', 'sync'}));
-if length(lsl_nirsevents)~= length(oxy4_nirsevents)
+if height(lsl_nirsevents)~= length(oxy4_nirsevents)
   warning('number of events in nirs data was not as expected')
 end
-if any(abs([lsl_nirsevents(:).timestamp]-lsl_nirsevents(1).timestamp-([oxy4_nirsevents(:).sample]-oxy4_nirsevents(1).sample)/50)>0.04)
-  warning('some events of .oxy4 file differed with more than 40 msec with the lsl stream (max difference = %f)', max(abs([lsl_nirsevents(:).timestamp]-lsl_nirsevents(1).timestamp-([oxy4_nirsevents(:).sample]-oxy4_nirsevents(1).sample)/50)))
+if any(abs([lsl_nirsevents.onset(:)]-lsl_nirsevents.onset(1)-([oxy4_nirsevents(:).sample]-oxy4_nirsevents(1).sample)/50)>0.04)
+  warning('some events of .oxy4 file differed with more than 40 msec with the lsl stream (max difference = %f)', max(abs([lsl_nirsevents.onset(:)]-lsl_nirsevents.onset(1)-([oxy4_nirsevents(:).sample]-oxy4_nirsevents(1).sample)/50)))
 else
-  fprintf('maximum delay between .oxy4 file and lsl stream was %.03f seconds \n',  max(abs([lsl_nirsevents(:).timestamp]-lsl_nirsevents(1).timestamp-([oxy4_nirsevents(:).sample]-oxy4_nirsevents(1).sample)/50)))
+  fprintf('maximum delay between .oxy4 file and lsl stream was %.03f seconds \n',  max(abs([lsl_nirsevents.onset(:)]-lsl_nirsevents.onset(1)-([oxy4_nirsevents(:).sample]-oxy4_nirsevents(1).sample)/50)))
 end
 figure; plot([oxy4_nirsevents.sample], ones(size(oxy4_nirsevents)), '+'); title('nirs events');
+% convert events to table
+fsample=data_combi.fsample;
+nirs_events=struct2table(oxy4_nirsevents);
+onset=([oxy4_nirsevents(:).sample]'-1)/fsample;
+sample=[oxy4_nirsevents(:).sample]';
+onset_corrected=onset(:)-lsl_nirsevents.correction(:);
+duration=zeros(length(onset),1);
+type=repmat({'sync-event'}, size(onset));
+value={oxy4_nirsevents(:).value}';
+nirs_events=table(onset, sample, onset_corrected, duration, type, value);
 % save events
-save(fullfile(root_dir, 'processed', sub, 'nirs', sprintf('sub-%s_task-gait_rec-%s_events.mat', fparts.sub, fparts.rec)), 'data_combi')
-
+save(fullfile(root_dir, 'processed', sub, 'nirs', sprintf('sub-%s_task-gait_rec-%s_events.mat', fparts.sub, fparts.rec)), 'nirs_events')
 
 % split in runs
 run=[];
 start_runs=oxy4_nirsevents(contains({oxy4_nirsevents.value}, 'start_run'));
 stop_runs=oxy4_nirsevents(contains({oxy4_nirsevents.value}, 'stop_run'));
-fsample=data_combi.fsample;
 for r=runs
   % data
   cfg=[];
@@ -90,9 +102,9 @@ save(fullfile(root_dir, 'processed', sub, sprintf('sub-%s_run.mat', ID)), 'run')
 %% load motion data
 % select the corresponding lsl events
 fprintf('\n.........loading motion data.........\n')
-lslstream='Digital Triggers @ lsldert04';
-start_runs=ft_filter_event(xdfevent, 'type', lslstream, 'value', 'TTL_on');
-stop_runs=ft_filter_event(xdfevent, 'type', lslstream, 'value', 'TTL_off');
+lslstream='lsldert4_events';
+start_runs=lsl_events(contains(lsl_events.type, lslstream) & contains(lsl_events.value, 'TTL_on'),:);
+stop_runs=lsl_events(contains(lsl_events.type, lslstream) & contains(lsl_events.value, 'TTL_off'),:);
 
 for r=runs
   fprintf('\n LOADING RUN %d \n', r)
@@ -105,24 +117,25 @@ for r=runs
   cfg.datafile=fullfile(mvnxfile.folder, mvnxfile.name);
   data_raw=ft_preprocessing(cfg);
   % check duration of recording
-  if abs((stop_runs(r).timestamp-start_runs(r).timestamp)-data_raw.time{1}(end))>0.1
-    if (stop_runs(r).timestamp-start_runs(r).timestamp)-data_raw.time{1}(end)<0
-      warning('duration of .mvnx file run %d differed with more than 100 msec of what was expected. .mvnx was %d seconds longer than expected.', r, abs((stop_runs(r).timestamp-start_runs(r).timestamp)-data_raw.time{1}(end)))
+  if abs((stop_runs.onset(r)-start_runs.onset(r))-data_raw.time{1}(end))>0.1
+    if (stop_runs.onset(r)-start_runs.onset(r))-data_raw.time{1}(end)<0
+      warning('duration of .mvnx file run %d differed with more than 100 msec of what was expected. .mvnx was %d seconds longer than expected.', r, abs((stop_runs.onset(r)-start_runs.onset(r))-data_raw.time{1}(end)))
     else
-      warning('duration of .mvnx file run %d differed with more than 100 msec of what was expected. .mvnx was %d seconds shorter than expected.', r, abs((stop_runs(r).timestamp-start_runs(r).timestamp)-data_raw.time{1}(end)))
+      warning('duration of .mvnx file run %d differed with more than 100 msec of what was expected. .mvnx was %d seconds shorter than expected.', r, abs((stop_runs.onset(r)-start_runs.onset(r))-data_raw.time{1}(end)))
     end
   else
-    fprintf('maximum drift between the .mvnx file run %d and lsl stream of %.03f seconds \n', r,abs((stop_runs(r).timestamp-start_runs(r).timestamp)-data_raw.time{1}(end)))
+    fprintf('maximum drift between the .mvnx file run %d and lsl stream of %.03f seconds \n', r,abs((stop_runs.onset(r)-start_runs.onset(r))-data_raw.time{1}(end)))
   end
   % check data by plotting trajectory of COM
   idx_COM=match_str(data_raw.label, {'seg_COM_centerOfMass_X','seg_COM_centerOfMass_Y','seg_COM_centerOfMass_Z'});
   figure; plot3(data_raw.trial{1}(idx_COM(1),:), data_raw.trial{1}(idx_COM(2),:),data_raw.trial{1}(idx_COM(3),:), '.'); view(2);title(sprintf('gait trajectory of run %d', r)); xlabel('X'); ylabel('Y')
   % make events
   onset = [0 data_raw.time{1}(end)]';
+  onset_corrected = [0-start_runs.correction(r) data_raw.time{1}(end)-stop_runs.correction(r)]';
   duration = [0 0]';
   type={'sync-event', 'sync-event'}';
   value = {'start_run', 'stop_run'}';
-  run_events=table(onset, duration, type, value);
+  run_events=table(onset, onset_corrected, duration, type, value);
   % save data and events
   run(r).data_motion.data_raw=data_raw;
   [~, n, ~]=fileparts(mvnxfile.name);
@@ -137,7 +150,7 @@ save(fullfile(root_dir, 'processed', sub, sprintf('sub-%s_run.mat', ID)), 'run')
 %% load video data
 fprintf('\n.........loading video data.........\n')
 acq={'mobile', 'begin', 'end'};
-lslstream={'Digital Triggers @ lsldert01', 'Digital Triggers @ lsldert02', 'Digital Triggers @ lsldert03'};
+lslstream={'lsldert1_events', 'lsldert2_events', 'lsldert3_events'};
 
 for a=1:length(acq)
   fprintf('LOADING CAM %s \n', acq{a})
@@ -149,57 +162,53 @@ for a=1:length(acq)
   end
   [dat, fsample, onset, value] = audio_trigger_detect(fullfile(mp4file.folder,mp4file.name));
   % check with the lslstream
-  lsl_videoevents=xdfevent(contains({xdfevent.type}, lslstream{a}) & contains({xdfevent.value}, {'start_run', 'stop_run', 'start_block', 'stop_block', 'sync'}));
-  if length(lsl_videoevents)~=length(onset)
+  lsl_videoevents=lsl_events(contains(lsl_events.type, lslstream{a}) & contains(lsl_events.value, {'start_run', 'stop_run', 'start_block', 'stop_block', 'sync'}),:);
+  if height(lsl_videoevents)~=length(onset)
     warning('cam %s did not contain the same amount of events as the lsl stream', acq{a})
   end
-  if any(abs(onset-onset(1)- ([lsl_videoevents(:).timestamp]'-lsl_videoevents(1).timestamp))>0.1)
-    warning('some events of video recording of cam %s differed with more than 100 msec with the lsl stream (max difference = %f)', max(abs(onset-onset(1)- ([lsl_videoevents(:).timestamp]'-lsl_videoevents(1).timestamp))))
+  if any(abs(onset-onset(1)-([lsl_videoevents.onset(:)]-lsl_videoevents.onset(1)))>0.1)
+    warning('some events of video recording of cam %s differed with more than 100 msec with the lsl stream (max difference = %f)', max(abs(onset-onset(1)- ([lsl_videoevents.onset(:)]-lsl_videoevents.onset(1)))))
   else
-    fprintf('maximum drift between video file cam %s and lsl stream of %.03f seconds \n', acq{a},max(abs(onset-onset(1)- ([lsl_videoevents(:).timestamp]'-lsl_videoevents(1).timestamp)))) 
+    fprintf('maximum drift between video file cam %s and lsl stream of %.03f seconds \n', acq{a},max(abs(onset-onset(1)- ([lsl_videoevents.onset(:)]-lsl_videoevents.onset(1))))) 
   end
-  if ~all(strcmp(value,{lsl_videoevents.value}'))
+  if ~all(strcmp(value,lsl_videoevents.value))
     warning('values of the video events were different than the lsl stream')
   end
+  onset_corrected=onset-lsl_videoevents.correction(:);
   duration = zeros(size(onset));
   type=repmat({'sync-event'}, size(onset));
-  video_events = table(onset, duration, type, value);
+  video_events = table(onset, onset_corrected, duration, type, value);
   % save events
   save(fullfile(root_dir, 'processed', sub, 'video', sprintf('sub-%s_task-gait_acq-%s_events.mat', sub, acq{a})), 'video_events');
 end
 
 %% save events
 % sync-events
-lslstream='lsldert04'; 
-lsl_syncevents=xdfevent(contains({xdfevent.type}, lslstream)&contains({xdfevent.value}, {'start_run', 'stop_run', 'start_block', 'stop_block', 'sync'}));
-start_runs=lsl_syncevents(contains({lsl_syncevents.value}, 'start_run'));
-stop_runs=lsl_syncevents(contains({lsl_syncevents.value}, 'stop_run'));
+lslstream='lsldert4_events'; 
+lsl_syncevents=lsl_events(contains(lsl_events.type, lslstream)&contains(lsl_events.value, {'start_run', 'stop_run', 'start_block', 'stop_block', 'sync'}),:);
+start_runs=lsl_syncevents(contains(lsl_syncevents.value, 'start_run'),:);
+stop_runs=lsl_syncevents(contains(lsl_syncevents.value, 'stop_run'),:);
 % big table
-events=struct2table(lsl_syncevents);
-events.onset=events.timestamp(:)-start_runs(1).timestamp; % relative to first start_run
+events=lsl_syncevents;
+events.onset=events.onset(:)-start_runs.onset(1); % relative to first start_run
 events.type(:)=repmat({'lslevent'},height(events),1);
-events.duration=zeros(height(events),1);
-events=events(:,[7 6 1 2]); % only retain onset-duration-type-value
 % split into runs
 for r=runs
-  run_events=ft_filter_event(lsl_syncevents, 'mintimestamp', start_runs(r).timestamp, 'maxtimestamp', stop_runs(r).timestamp);
-  run_events=struct2table(run_events);
-  run_events.onset=run_events.timestamp(:)-start_runs(r).timestamp;
+  run_events=lsl_syncevents((lsl_syncevents.onset(:)>=start_runs.onset(r) & lsl_syncevents.onset(:)<=stop_runs.onset(r)),:);
+  run_events.onset=run_events.onset(:)-start_runs.onset(r);
   run_events.type(:)=repmat({'lslevent'},height(run_events),1);
-  run_events.duration=zeros(height(run_events),1);
-  run_events=run_events(:,[7 6 1 2]); % only retain onset-duration-type-value
   run(r).events = run_events; % store in runs
 end
 
 % FOG events
-lslstream='lsldert01';
-lsl_FOGevents=xdfevent(contains({xdfevent.type}, lslstream)&contains({xdfevent.value}, {'FOG'}));
-if ~isempty(lsl_FOGevents)
-  FOGevents=struct2table(lsl_FOGevents);
-  FOGevents.onset=FOGevents.timestamp(:)-start_runs(1).timestamp; % relative to first start_run
-  FOGevents.type(:)=repmat({'lslevent'},height(FOGevents),1);
-  FOGevents.duration=zeros(height(FOGevents),1);
-  FOGevents=FOGevents(:,[7 6 1 2]); % only retain onset-duration-type-value
+lsldert1_timestamps=lsl_correct_lsl_timestamps(REC.lsldert1_events);
+lsl_FOGevents_idx=find(endsWith(REC.lsldert1_events.Data, 'FOG'));
+if ~isempty(lsl_FOGevents_idx)
+  onset=lsldert1_timestamps(lsl_FOGevents_idx)'-start_runs.onset(1);
+  duration=zeros(size(onset));
+  type=repmat({'lslevent'},length(onset),1);
+  value=repmat({'FOG'}, length(onset),1);
+  FOGevents=table(onset, duration, type, value);
   % add to big table
   events=[events; FOGevents];
 end
--- test_delay.m
+++ test_delay.m
@@ -1,50 +1,81 @@
-function test_delay(REC)
-%% 
+function lsl_events=test_delay(REC)
+ 
+%% load events
 events{1}=[REC.trig_events.timestamp];
-idx_events=find(endsWith(REC.lsldert0_events.Data, {'start_run', 'stop_run', 'start_block', 'stop_block', 'sync'}));
+idx_events=find(endsWith(REC.lsldert0_events.Data, {'TTL_on', 'TTL_off', 'start_run', 'stop_run', 'start_block', 'stop_block', 'sync'}));
 lsldert0_events=lsl_correct_lsl_timestamps(REC.lsldert0_events);
 events{2}=lsldert0_events(idx_events);
-idx_events=find(endsWith(REC.lsldert1_events.Data, {'start_run', 'stop_run','start_block', 'stop_block', 'sync'}));
+idx_events=find(endsWith(REC.lsldert1_events.Data, {'TTL_on', 'TTL_off', 'start_run', 'stop_run','start_block', 'stop_block', 'sync'}));
 lsldert1_events=lsl_correct_lsl_timestamps(REC.lsldert1_events);
 events{3}=lsldert1_events(idx_events);
-idx_events=find(endsWith(REC.lsldert2_events.Data, {'start_run', 'stop_run','start_block', 'stop_block', 'sync'}));
+idx_events=find(endsWith(REC.lsldert2_events.Data, {'TTL_on', 'TTL_off', 'start_run', 'stop_run','start_block', 'stop_block', 'sync'}));
 lsldert2_events=lsl_correct_lsl_timestamps(REC.lsldert2_events);
 events{4}=lsldert2_events(idx_events);
-idx_events=find(endsWith(REC.lsldert3_events.Data, {'start_run', 'stop_run','start_block', 'stop_block', 'sync'}));
+idx_events=find(endsWith(REC.lsldert3_events.Data, {'TTL_on', 'TTL_off', 'start_run', 'stop_run','start_block', 'stop_block', 'sync'}));
 lsldert3_events=lsl_correct_lsl_timestamps(REC.lsldert3_events);
 events{5}=lsldert3_events(idx_events);
-idx_events=find(endsWith(REC.lsldert4_events.Data, {'start_run', 'stop_run','start_block', 'stop_block', 'sync'}));
+idx_events=find(endsWith(REC.lsldert4_events.Data, {'TTL_on', 'TTL_off', 'start_run', 'stop_run','start_block', 'stop_block', 'sync'}));
 lsldert4_events=lsl_correct_lsl_timestamps(REC.lsldert4_events);
 events{6}=lsldert4_events(idx_events);
  
- 
-figure; title('timestamps for all events'); hold on
+%% plot delays 
+figure;
+subplot(3,1,1); title('timestamps for all events'); hold on
 for i=1:6
   plot(events{i}, ones(size(events{i}))*i, '+')
 end
-ylim([-23 30])
+ylim([-3 10])
 legend({'trigger', 'lsldert0', 'lsldert1', 'lsldert2', 'lsldert3', 'lsldert4'})
 
-figure; title('delay between creating the trigger and sending it via lsldert0'); hold on;
-plot(events{1}, events{2}-events{1})
-ylabel('time delay (sec.)'); xlabel('timestamps');
-ylim([0 0.1]);
+% figure; title('delay between creating the trigger and sending it via lsldert0'); hold on;
+% plot(events{1}, events{2}-events{1})
+% ylabel('time delay (sec.)'); xlabel('timestamps');
+% ylim([0 0.1]);
  
-figure; title('delay of timestamp relative to when the trigger was sent via lsldert0'); hold on;
+subplot(3,1,2); title('delay of timestamp relative to when the trigger was sent via lsldert0'); hold on;
 for i=3:6
   plot(events{2}, events{i}-events{2})
 end
 legend({'lsldert1', 'lsldert2', 'lsldert3', 'lsldert4'})
 ylabel('time delay (sec.)'); xlabel('timestamps');
-ylim([0 0.1]);
+ylim([0 0.05]);
 
-figure; title('delay between raspberry pies (relative to lsldert1)'); hold on;
-for i=4:6
-  plot(events{3}, events{i}-events{3})
+subplot(3,1,3); title('delay between raspberry pies (relative to lsldert4)'); hold on;
+for i=3:5
+  plot(events{6}, events{i}-events{6})
 end
-legend({'lsldert2', 'lsldert3', 'lsldert4'})
+legend({'lsldert1', 'lsldert2', 'lsldert3'})
 ylabel('time delay (sec.)'); xlabel('timestamps');
-ylim([-0.05 0.05]);
+ylim([0 0.05]);
 
+%% calculate corrections relative to lsldert4
+% lsldert04
+idx_events0=find(endsWith(REC.lsldert0_events.Data, {'TTL_on', 'TTL_off', 'start_run', 'stop_run', 'start_block', 'stop_block', 'sync'}));
+lsl_timestamps0=lsl_correct_lsl_timestamps(REC.lsldert0_events);
+lsl_timestamps0=lsl_timestamps0(idx_events0)';
+correction=zeros(length(lsl_timestamps0), 1);
+duration=zeros(length(lsl_timestamps0), 1);
+type=repmat({'lsldert0_events'},length(lsl_timestamps0),1);
+% start making a table
+lsl_events=table(lsl_timestamps0, correction, duration, type, {REC.lsldert0_events.Data{idx_events0}}', 'VariableNames', {'onset', 'correction', 'duration', 'type', 'value'})
+% loop over other lsl streams and calculate corrections relative to
+% lsldert00
+lsl_streams={'lsldert1_events', 'lsldert2_events', 'lsldert3_events', 'lsldert4_events'};
+for i=1:4
+  idx_eventsi=find(endsWith(REC.(lsl_streams{i}).Data, {'TTL_on', 'TTL_off', 'start_run', 'stop_run', 'start_block', 'stop_block', 'sync'}));
+  lsl_timestampsi=lsl_correct_lsl_timestamps(REC.(lsl_streams{i}));
+  lsl_timestampsi=lsl_timestampsi(idx_eventsi)';
+  if length(lsl_timestampsi)~= length(lsl_timestamps0)
+    warning('%s has not the same number of events as lsldert0_events. Not calculating corrections', lsl_streams{i})
+    continue
+  end
+  lsl_corri=lsl_timestampsi-lsl_timestamps0;
+  duration=zeros(length(lsl_timestampsi),1);
+  type=repmat({lsl_streams{i}},length(lsl_timestampsi),1);
+  % make table
+  tablei=table(lsl_timestampsi, lsl_corri, duration, type, {REC.(lsl_streams{i}).Data{idx_eventsi}}', 'VariableNames', {'onset', 'correction', 'duration', 'type', 'value'});
+  % concatenate with lsl_events
+  lsl_events=[lsl_events; tablei];
+end
 
 
