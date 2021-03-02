72c8b648b0080649a3a1f100d5f5fbccf4f4652a
helenacockx <h.cockx@donders.ru.nl>
2021-03-02 08:54:50 +0100

    update sub_info

--- load_sub_info.m
+++ load_sub_info.m
@@ -1,6 +1,10 @@
 function sub_info=load_sub_info(ID);
 
 switch ID
+  case 'PD10'
+    sub_info.runs=[1:4];
+    sub_info.num_run=4;
+    sub_info.num_block=12;     
   case 'PD11'
     sub_info.runs=[1:5];
     sub_info.num_run=5;
@@ -17,6 +21,14 @@ switch ID
     sub_info.runs=[1:5];
     sub_info.num_run=5;
     sub_info.num_block=12;
+  case 'PD35'
+    sub_info.runs=[1:4];
+    sub_info.num_run=4;
+    sub_info.num_block=11;
+  case 'PD46'
+    sub_info.runs=[1:4];
+    sub_info.num_run=4;
+    sub_info.num_block=12;
   case 'PD57'
     sub_info.runs=[1:4];
     sub_info.num_run=4;
@@ -32,7 +44,11 @@ switch ID
   case 'PD76'
     sub_info.runs=[1:4];
     sub_info.num_run=4;
-    sub_info.num_block=12;       
+    sub_info.num_block=12; 
+  case 'PD77'
+    sub_info.runs=[1:4];
+    sub_info.num_run=4;
+    sub_info.num_block=12;
   case 'PD90'
     sub_info.runs=[1:6];
     sub_info.num_run=6;
@@ -48,11 +64,43 @@ switch ID
   case 'HC13'
     sub_info.runs=[1:4];
     sub_info.num_run=4;
+    sub_info.num_block=12; 
+  case 'HC19'
+    sub_info.runs=[1:4];
+    sub_info.num_run=4;
+    sub_info.num_block=12;  
+  case 'HC25'
+    sub_info.runs=[1:4];
+    sub_info.num_run=4;
+    sub_info.num_block=12;
+  case 'HC29'
+    sub_info.runs=[1:4];
+    sub_info.num_run=4;
+    sub_info.num_block=12; 
+  case 'HC33'
+    sub_info.runs=[1:4];
+    sub_info.num_run=4;
+    sub_info.num_block=12;
+  case 'HC34'
+    sub_info.runs=[1:4];
+    sub_info.num_run=4;
     sub_info.num_block=12;     
+  case 'HC35'
+    sub_info.runs=[1:4];
+    sub_info.num_run=4;
+    sub_info.num_block=12;
   case 'HC42'
     sub_info.runs=[1:6];
     sub_info.num_run=6;
     sub_info.num_block=16;
+  case 'HC57'
+    sub_info.runs=[1:6];
+    sub_info.num_run=6;
+    sub_info.num_block=17;    
+  case 'HC60'
+    sub_info.runs=[1:4];
+    sub_info.num_run=4;
+    sub_info.num_block=12;     
   case 'HC66'
     sub_info.runs=[1:4];
     sub_info.num_run=4;
@@ -61,4 +109,12 @@ switch ID
     sub_info.runs=[1:4];
     sub_info.num_run=4;
     sub_info.num_block=12;     
+  case 'HC90'
+    sub_info.runs=[1:4];
+    sub_info.num_run=4;
+    sub_info.num_block=12;    
+   case 'HC91'
+    sub_info.runs=[1:4];
+    sub_info.num_run=4;
+    sub_info.num_block=12;       
 end
